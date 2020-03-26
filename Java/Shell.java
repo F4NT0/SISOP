2df13a42083add2a3cb77f0cd3d4559b5607d25c
gabriel.stundner <gabriel.stundner@acad.pucrs.br>
2020-03-26 15:23:43 -0300

    command exit working

--- Java/Shell.java
+++ Java/Shell.java
@@ -8,10 +8,18 @@ import java.awt.event.*;
 
 
 public class Shell implements KeyListener {
+
+/****************************************************
+ *     VARIÁVEIS GLOBAIS PARA UTILIZAR NAS FUNÇÕES
+ ***************************************************/
+
     private JTextArea output;
     private JTextField input;
     private JFrame f;
 
+/***************************************
+ *    TELA DO TERMINAL COM JAVA SWING
+ **************************************/
 
     public void screen(){
 
@@ -21,12 +29,16 @@ public class Shell implements KeyListener {
         output.setBounds(0,0,930,400);//(posicaoX,posicaoY,largura,altura)
         output.setBackground(Color.black);
         output.setForeground(Color.green);
+        output.setFocusable(false);
         
         // Área de Entrada de texto
         input = new JTextField();
         input.setBounds(0, 406, 930, 40);
         input.setBackground(Color.black);
         input.setForeground(Color.green);
+        input.setFocusable(true);
+        input.addKeyListener(this);
+        input.setFocusTraversalKeysEnabled(false);
 
         f=new JFrame();
         f.setSize(930,496);
@@ -34,23 +46,44 @@ public class Shell implements KeyListener {
         f.add(output);
         f.add(input);
         f.setLayout(null);
-        f.addKeyListener(this);
-        f.setFocusable(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
     }
 
-    // Métodos importados da classe KeyListener
+/****************************************************
+ *    MÉTODOS IMPORTADOS DO KEYLISTENER PARA TECLADO
+ ****************************************************/
+
+    // Se clicar na tecla
     @Override
     public void keyPressed(KeyEvent e){
         if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
             f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
         }
+        if(e.getKeyCode() == KeyEvent.VK_ENTER){
+            String saida = input.getText();
+            commands(saida);
+        }
     }
+    // Outros não foram utilizados
     public void keyReleased(KeyEvent e){}
     public void keyTyped(KeyEvent e){}
 
+/********************************************
+ *  MÉTODO DE CONTROLE DE COMANDOS ENTRADOS
+ *******************************************/
 
+    public void commands(String text){
+        if(text.equals("exit")){
+            f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
+        }
+    }
+
+
+
+/*********************
+ *  TESTE COM O MAIN
+ ********************/
 
     public static void main(String[] args) {
        Shell shell = new Shell();
