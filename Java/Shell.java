05835a93ecbcc5befc818725a0bbebd802d6ecd9
gabriel.stundner <gabriel.stundner@acad.pucrs.br>
2020-03-26 13:59:06 -0300

    shell close sistem

--- Java/Shell.java
+++ Java/Shell.java
@@ -8,52 +8,45 @@ import java.awt.event.*;
 
 public class Shell implements KeyListener {
 
-    // Construção da Tela
-    public static void screen(){
-    }
+    //Variáveis Globais
 
+
+    public static void screen(){
+
+
+        // Área de Texto
+        JTextArea output = new JTextArea();
+        //output.setSize(930,430);
+        output.setBounds(0,0,930,400);//(posicaoX,posicaoY,largura,altura)
+        output.setBackground(Color.black);
+        output.setForeground(Color.green);
+        
+        // Área de Entrada de texto
+        JTextField input = new JTextField();
+        input.setBounds(0, 406, 930, 40);
+        input.setBackground(Color.black);
+        input.setForeground(Color.green);
+
+        
+        JFrame f=new JFrame();
+        f.setSize(930,496);
+        f.getContentPane().setBackground(Color.gray);
+        f.add(output);
+        f.add(input);
+        f.setLayout(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
 
     // Métodos importados da classe KeyListener
-    public void keyPressed(KeyEvent e){
-        if(e.getKeyCode() == KeyEvent.VK_ENTER){
-
-        }
-    }
+    @Override
+    public void keyPressed(KeyEvent e){}
     public void keyReleased(KeyEvent e){}
     public void keyTyped(KeyEvent e){}
 
-    // Método de Ações do Programa
-    // public void actionPerformed(ActionEvent e){
-    //     if(e.getKeyCode() == KeyEvent.VK_ENTER){
-            
-    //     }
-    // }
 
     public static void main(String[] args) {
-        //TODO: colocar keylistener no programa
-            // Área de Texto
-            JTextArea output = new JTextArea();
-            //output.setSize(930,430);
-            output.setBounds(0,0,930,400);//(posicaoX,posicaoY,largura,altura)
-            output.setBackground(Color.black);
-            output.setForeground(Color.green);
-            
-            // Área de Entrada de texto
-            JTextField input = new JTextField();
-            input.setBounds(0, 406, 930, 40);
-            input.setBackground(Color.black);
-            input.setForeground(Color.green);
-
-
-    
-            JFrame f=new JFrame();
-            f.setSize(930,496);
-            f.getContentPane().setBackground(Color.gray);
-            f.add(output);
-            f.add(input);
-            f.setLayout(null);
-            f.setVisible(true);
-        
+        screen();
     }
 }
 
