251440149ae7e5e9a743a496258608b1ea3d33a4
gabriel.stundner <gabriel.stundner@acad.pucrs.br>
2020-03-26 16:36:27 -0300

    trying to read programs

--- Java/Shell.java
+++ Java/Shell.java
@@ -2,10 +2,13 @@
  * SHELL DE INTERAÇÃO DO USUÁRIO COM O PROGRAMA
  **********************************************/
 
+// Info das Telas
 import javax.swing.*;
 import java.awt.Color;
 import java.awt.event.*;
-
+// Lendo arquivo simples
+import java.util.ArrayList;
+import java.util.List;
 
 public class Shell implements KeyListener {
 
@@ -56,37 +59,44 @@ public class Shell implements KeyListener {
 
     // Se clicar na tecla
     @Override
-    public void keyPressed(KeyEvent e){
-        if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+    public void keyPressed(final KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
             f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
         }
-        if(e.getKeyCode() == KeyEvent.VK_ENTER){
-            String saida = input.getText();
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            final String saida = input.getText();
             commands(saida);
         }
     }
-    // Outros não foram utilizados
-    public void keyReleased(KeyEvent e){}
-    public void keyTyped(KeyEvent e){}
 
-/********************************************
- *  MÉTODO DE CONTROLE DE COMANDOS ENTRADOS
- *******************************************/
+    // Outros não foram utilizados
+    public void keyReleased(final KeyEvent e) {
+    }
+
+    public void keyTyped(final KeyEvent e) {
+    }
+
+    /********************************************
+     * MÉTODO DE CONTROLE DE COMANDOS ENTRADOS
+     *******************************************/
 
     public void commands(String text){
-        if(text.equals("exit")){
+        if (text.equals("exit")) {
             f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
+        }
+        if (text.equals("ls")) {
+            List<String> allFiles = new ArrayList<String>();
+            String filename = "programs.txt";
+            // TODO: ler o arquivo com os nomes dos programas
         }
     }
 
+    /*********************
+     * TESTE COM O MAIN
+     ********************/
 
-
-/*********************
- *  TESTE COM O MAIN
- ********************/
-
-    public static void main(String[] args) {
-       Shell shell = new Shell();
+    public static void main(final String[] args) {
+        final Shell shell = new Shell();
        shell.screen();
     }
 }
