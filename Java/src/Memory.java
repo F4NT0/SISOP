90e4a74f22aeffca00e07e6cf7a756cd02291eaf
luizguerra <luizpedrofg@gmail.com>
2020-03-30 20:18:29 -0300

    comitao

--- Java/src/Memory.java
+++ Java/src/Memory.java
@@ -1,29 +1,41 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.List;
 import java.util.ArrayList;
 
-final class Memory <T> {
+final class Memory {
 
     private final Integer memorySize = 1024;
-    private List <T> memoryArray;
+    private List <Object> memoryArray;
     private Integer programCounter;
-
-
 
     public Memory() {
         memoryArray = new ArrayList<>(memorySize);
+        for (int i = 0; i < memorySize; i++) { memoryArray.set(i, null); }
         programCounter = 0;
     }
 
-    public void set (int index, T value) {
+    public void set (int index, Object value) {
         memoryArray.set(index, value);
     }
 
-    public void add (T value) {
+    public void add (Object value) throws OutOfMemoryError {
+        if (memoryArray.size() >= memorySize) {
+            throw new OutOfMemoryError("Tried to add Object " + value + " while memory is full.");
+        }
         memoryArray.add(value);
     }
 
-    public void remove (int index) {
+
+    public void remove (int index) throws IndexOutOfBoundsException {
+        if (memoryArray.size() < index) {
+            throw new IndexOutOfBoundsException("Tried to acess ");
+        }
         memoryArray.remove(index);
+    }
+
+    public void remove(Object object) {
+        this.memoryArray.remove(object);
     }
 
     public void createPartition () {
@@ -33,7 +45,9 @@ final class Memory <T> {
 
     }
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public List array() {
+        return this.memoryArray;
     }
+
+
 }
---
+++ Java/src/OPCODE.java
@@ -0,0 +1,19 @@
+//public class ProcessorRegister <T>{
+
+public final class OPCODE {
+
+    private String OPCODE;
+
+    public OPCODE (String code) {
+        this.OPCODE = code;
+    }
+
+    public void execute () {
+
+    }
+
+    private void JMP () {
+//        example
+    }
+
+}
