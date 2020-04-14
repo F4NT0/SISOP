202dce1bcd5e5a508f7d9bc76f90e0a51e3d888b
Gabriel Fanto Stundner <gabriel.stundner@acad.pucrs.br>
2020-04-14 11:52:56 -0300

    Update teste.sh

--- ci_cd/teste.sh
+++ ci_cd/teste.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-$ docker build -f Dockerfile -t demo/oracle-java:8 .
 cd ..
 cd Java/src/
-docker run --rm -v $PWD:/app -w /app demo/oracle-java:8 javac *.java
-docker run --rm -v $PWD:/app -w /app demo/oracle-java:8 java Teste_Functions
+javac *.java
+java Teste_Functions
+echo "TESTE FINALIZADO"
