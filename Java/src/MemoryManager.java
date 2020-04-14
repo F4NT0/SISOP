63ceeeb821efba0b1d4610d10364b388a1be04ea
lucashleal <lucas.hanauer@outlook.com>
2020-04-14 10:31:52 -0300

    Comeco da implementacao do gerente de memoria

---
+++ Java/src/MemoryManager.java
@@ -0,0 +1,9 @@
+import java.util.Queue;
+
+public class MemoryManager {
+
+    private Object [][] validIndex = new Object [1024][2];
+    private Queue processQueue;
+    private final int [] partitionsIndex = {15, 47, 79, 143, 207, 335, 591};
+
+}
