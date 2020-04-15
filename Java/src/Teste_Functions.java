738e0f011926e1d59c500555f32b26436e162c5f
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-04-14 21:28:26 -0300

    CI CD TEST UPDATED

--- Java/src/Teste_Functions.java
+++ Java/src/Teste_Functions.java
@@ -5,28 +5,24 @@
 public class Teste_Functions{
     public static void main(String[] args){
         // Criando um CPU
-        Cpu cpu = new Cpu();
+        //Cpu cpu = new Cpu();
 
         // ---------- Teste de Leitura --------------
-        cpu.loadProgram("prog.txt");
-        System.out.println("PC: " + cpu.getPc());
-        System.out.println("Size: " + cpu.getProgramSize());
-        cpu.runningProgram(cpu.getProgramSize());
-        System.out.println("PC: " + cpu.getPc());
-        cpu.testMemory();
-        String finalValues = cpu.finalValues();
-        System.out.println(finalValues);
-        //cpu.clearMemory();
-        cpu.loadProgram("prog2.txt");
-        System.out.println("PC: " + cpu.getPc());
-        System.out.println("Size: " + cpu.getProgramSize());
-        cpu.runningProgram(cpu.getProgramSize());
-        System.out.println("PC: " + cpu.getPc());
-        cpu.testMemory();
-        System.out.println("Final Values");
-        String finalValues2 = cpu.finalValues();
-        System.out.println(finalValues2);
-
+        // System.out.println("----------------------------");
+        // System.out.println("LEITURA DO PROGRAMA PROG.TXT");
+        // System.out.println("----------------------------");
+        // System.out.println("\n");
+        // cpu.loadProgram("prog.txt");
+        // System.out.println("PC: " + cpu.getPc());
+        // System.out.println("Size: " + cpu.getProgramSize());
+        // cpu.runningProgram(cpu.getProgramSize());
+        // System.out.println("PC: " + cpu.getPc());
+        // cpu.testMemory();
+        // System.out.println("VALORES ARMAZENADOS NOS REGISTRADORES");
+        // String finalValues = cpu.finalValues();
+        // System.out.println(finalValues);
+       
+            
         // ----------- Teste do JMP ---------------
             // Funcao testeJMP = new Funcao();
             // testeJMP.setOpcode("JMP");
