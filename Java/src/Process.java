56352713856dc7be1567b8d64b6aaa0c578cb268
luizguerra <luizpedrofg@gmail.com>
2020-04-14 21:55:31 -0300

    Process Object

---
+++ Java/src/Process.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Process {
+
+    private ProcessControlBlock processControlBlock;
+    private List<Funcao> functions;
+
+    public Process () {
+
+    }
+
+}
