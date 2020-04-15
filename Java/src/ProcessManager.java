54aa8dad36f1441bf62913262b178e8d3633b1ae
luizguerra <luizpedrofg@gmail.com>
2020-04-14 21:55:38 -0300

    Process Manager

---
+++ Java/src/ProcessManager.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ProcessManager {
+
+    Queue <Process> queue;
+
+    public ProcessManager() {
+        this.queue = new LinkedList<>();
+    }
+
+
+
+}
