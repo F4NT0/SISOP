4347a34ae4466b7dbea27cf7b85f7d358563efa9
luizguerra <luizpedrofg@gmail.com>
2020-04-17 00:30:23 -0300

    Updated process

--- Java/src/Process.java
+++ Java/src/Process.java
@@ -4,10 +4,32 @@ import java.util.ArrayList;
 public class Process {
 
     private ProcessControlBlock processControlBlock;
-    private List<Funcao> functions;
+    private List<Funcao> functions; // program
+//    need to pull necessary program data
 
-    public Process () {
+    public Process (ArrayList<Funcao> program, Integer pcbLocationID) {
+        this.functions = program;
+        this.processControlBlock = new ProcessControlBlock(pcbLocationID);
+    }
 
+    void setPCBContextData(ProcessControlBlock.ContextData contextData) {
+        this.processControlBlock.setContextData(contextData);
+    }
+
+    public ProcessControlBlock getProcessControlBlock() {
+        return processControlBlock;
+    }
+
+    public void setProcessControlBlock(ProcessControlBlock processControlBlock) {
+        this.processControlBlock = processControlBlock;
+    }
+
+    public List<Funcao> getFunctions() {
+        return functions;
+    }
+
+    public void setFunctions(List<Funcao> functions) {
+        this.functions = functions;
     }
 
 }
