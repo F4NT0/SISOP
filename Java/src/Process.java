14e8105216f2f60fad328c74ce6cd13eae64768e
luizguerra <luizpedrofg@gmail.com>
2020-04-17 00:37:47 -0300

    Process external data

--- Java/src/Process.java
+++ Java/src/Process.java
@@ -1,15 +1,20 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Process {
 
     private ProcessControlBlock processControlBlock;
     private List<Funcao> functions; // program
 //    need to pull necessary program data
+//    should it be a hashmap, with variable name and its value?
+//    private Map<String, Object> data;
 
     public Process (ArrayList<Funcao> program, Integer pcbLocationID) {
         this.functions = program;
         this.processControlBlock = new ProcessControlBlock(pcbLocationID);
+//        data = new HashMap<>();
     }
 
     void setPCBContextData(ProcessControlBlock.ContextData contextData) {
