567c444ebf983475b1959cccd229b0971285daac
LuizGuerra <luizpedrofg@gmail.com>
2020-06-07 02:01:22 -0300

    Basic memory access implementation

--- Java/src/system/memory/MemoryAccess.java
+++ Java/src/system/memory/MemoryAccess.java
@@ -1,5 +1,11 @@
 package system.memory;
 
 public class MemoryAccess {
+    private MemoryManager manager;
+
+    public MemoryAccess() {
+        this.manager = new MemoryManager();
+    }
+
     
 }
