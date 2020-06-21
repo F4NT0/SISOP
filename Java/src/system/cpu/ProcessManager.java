4747ca4715746da0dc3deee903dc2cc17fd45008
LuizGuerra <luizpedrofg@gmail.com>
2020-06-21 20:57:04 -0300

    Removed empty process manager since there is another

--- Java/src/system/cpu/ProcessManager.java
+++
@@ -1,9 +0,0 @@
-package system.cpu;
-
-public class ProcessManager {
- public static void main(String[] args){
-				System.out.println("┌─────────────────────────┐");
-				System.out.println("׀ PROCESS MANAGER RODANDO ׀");
-				System.out.println("└─────────────────────────┘");
-	 }   
-}
