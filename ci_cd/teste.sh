ca9314d4df16e6c6ec9fcd056e4b52b48faacc8c
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-06-22 15:23:19 -0300

    running teste

--- ci_cd/teste.sh
+++ ci_cd/teste.sh
@@ -5,7 +5,7 @@ cd Java/src/
 mkdir compiled_program/
 
 # TESTES DEFINIDOS INTERNOS
-echo "${tput setaf 2}"
+echo -e "\033[0;32m"
 chmod +x TESTE.sh && ./TESTE.sh
-echo "${tput sgr0}"
+echo -e "\033[0m"
 
